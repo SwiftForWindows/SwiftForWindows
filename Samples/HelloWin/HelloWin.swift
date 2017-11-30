0f10f107f86ef58e7a67fc509058de7059d2952b
Han Sangjin <tinysun.net@gmail.com>
2017-12-01 07:29:49 +0900

    [sfw] For next version
    - Package for
        https://github.com/tinysun212/swift-windows/releases/tag/swift-mingw-20171201
        https://github.com/tinysun212/swift-corelibs-foundation/releases/tag/swift-mingw-20171201
    
    - Included the Foundation module

--- Samples/HelloWin/HelloWin.swift
+++ Samples/HelloWin/HelloWin.swift
@@ -2,4 +2,4 @@ import wx
 
 var app = wx.App()
 
-_ = wx.MessageDialog(wx.Window(), "Hello Window", "HelloWin", style:wx.OK).ShowModal()
+_ = wx.MessageDialog(wx.Window(), "Hello Window", "HelloWin", style:wx.OK).showModal()
